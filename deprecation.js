892124aa44471765b9f588fff9971b697ceda9c6
Kenan Sulayman <kenan@sly.mn>
2014-12-17 23:09:53 +0100

    Deprecation

---
+++ deprecation.js
@@ -0,0 +1,30 @@
+[
+	"",
+	"=================================",
+	"=================================",
+	"=========   DEPRECATED   ========",
+	"=================================",
+	"=================================",
+	"",
+	"Since this module was released,",
+	"Mollie B.V. published substantial",
+	"work to the node module. Therefore,",
+	"please migrate to the official",
+	"client in a foreseable future.",
+	"",
+	"This client internally uses the",
+	"offical client which shares the",
+	"same api, thus brings no real",
+	"migration cost.",
+	"",
+	"Thanks for your trust in the past,",
+	"Kenan Sulayman.",
+	"",
+	"=================================",
+	"================================="
+].forEach(function (a) {
+	a = ">> " + a
+	console.log(a)
+});
+
+process.exit(1)
--- mollie.js
+++ mollie.js
@@ -36,37 +36,4 @@
 	@link        https://www.mollie.nl
 */
 
-var app;
-
-var http = require("http"),
-     url = require("url"),
-      fs = require("fs");
-
-/*
-	Example app.
-*/
-
-app = http.createServer(function(request, response) {
-	var error, example, path;
-	path = url.parse(request.url).pathname;
-	
-	/*
-		Load requested example.
-	*/
-
-	try {
-		example = require("." + path);
-		return new example(request, response);
-	} catch (_error) {
-		error = _error;
-		console.error(error);
-		response.writeHead(200, {
-			"Content-Type": "text/html; charset=utf-8"
-		});
-		response.write('<a href="./1-new-payment">Try example 1</a><br>');
-		response.write('<a href="./4-ideal-payment">Try example 4</a>');
-		return response.end();
-	}
-});
-
-app.listen(8888);
+module.exports = require("mollie-api-node");
